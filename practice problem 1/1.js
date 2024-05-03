ef42155ada2d1ec88663739b704570f5fe6d092c
SHIM-JINSEO <90755177+SHIM-JINSEO@users.noreply.github.com>
2024-05-03 18:59:36 +0900

    Add files via upload

--- practice problem 1/1.js
+++ practice problem 1/1.js
@@ -4,16 +4,26 @@
 // 같으면 (a + b) * c is same as a + (b * c), 
 // 다르면 (a + b) * c is not same as a + (b * c)
 // 라고 출력하세요.
-
 // 사용되는 개념: 변수, 연산자, 조건문, 삼항연산자
 
 // 변수 a, b, c 만들기
-
+let a=2, b=3, c=4;
 // (a + b) * c 를 계산하고 다른 변수에 저장한 후 출력하기
-
+let n1;
+n1 = (a+b)*c;
 // a + (b * c) 를 계산하고 다른 변수에 저장한 후 출력하기
-
+let n2; 
+n2 = a + (b * c);
 // 조건문을 이용하여 둘을 비교하고 결과를 출력하기
-
+if (n1 === n2)
+    {
+        console.log('(a + b) * c is same as a + (b * c)');
+    }
+else
+    {
+        console.log('(a + b) * c is not same as a + (b * c)');
+    }
 // 삼항연산자를 이용하여 다시 둘을 비교하고 결과를 출력하기 
+let text = (n1 === n2)?'(a + b) * c is same as a + (b * c)':'(a + b) * c is not same as a + (b * c)';
 
+console.log(text);
